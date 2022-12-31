eb4b37ff5a7f8ef362d0d4453782b4931620daa5
Dima <Uchiha132132qqq@gmail.com>
2022-12-31 12:37:27 +0300

    feat: add modal to view client's avatar

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,4 +1,5 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import { ModalAvatar } from './ModalAvatar';
 import { WeekCounter } from './WeekCounter';
 
 interface IUserCard {
@@ -11,15 +12,18 @@ export const DefaultUserImage =
     'https://i.scdn.co/image/ab6775700000ee8549835514e2fac464191927c7';
 
 const UserCard: FC<IUserCard> = ({ userName, imgUrl, followers }) => {
+    const [modalActive, setModalActive] = useState(false);
+
     return (
         <div className="flex p-5">
-            <div className="mr-9">
-                <img
-                    src={imgUrl}
-                    alt="User icon "
-                    className="h-16 rounded-full"
-                />
-            </div>
+            <img
+                src={imgUrl}
+                alt="User icon"
+                className="h-16 rounded-full mr-9 cursor-pointer"
+                onClick={() => {
+                    setModalActive(true);
+                }}
+            />
             <div>
                 <div className="p-0.5 flex ">
                     <div className="font-semibold text-white text-lg mr-6 leading-6 text-shadow-md">
@@ -32,6 +36,9 @@ const UserCard: FC<IUserCard> = ({ userName, imgUrl, followers }) => {
                 </div>
                 <WeekCounter />
             </div>
+            {modalActive && (
+                <ModalAvatar img={imgUrl} isOpen={modalActive} handleCloseModal={setModalActive} />
+            )}
         </div>
     );
 };
